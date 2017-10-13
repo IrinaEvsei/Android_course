a331b21737c49800c57f11bbd58fb0009158716a
Irina Evsei <irinaevsei.ie@gmail.com>
2017-10-14 01:32:29 +0300

    Implemented the hor[Gizontal or[Gientat[Gion of the appl[Gicat[Gion

--- Task_1/app/src/main/java/com/example/irisha/task_1/FormActivity.java
+++ Task_1/app/src/main/java/com/example/irisha/task_1/FormActivity.java
@@ -3,7 +3,6 @@ package com.example.irisha.task_1;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
 
