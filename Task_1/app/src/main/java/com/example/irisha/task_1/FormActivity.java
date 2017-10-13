beddf79d304a94732367e49bfca9091a25dc377d
Irina Evsei <irinaevsei.ie@gmail.com>
2017-10-14 01:34:15 +0300

    Revert "Implemented the hor[Gizontal or[Gientat[Gion of the appl[Gicat[Gion"
    This reverts commit a331b21737c49800c57f11bbd58fb0009158716a.

--- Task_1/app/src/main/java/com/example/irisha/task_1/FormActivity.java
+++ Task_1/app/src/main/java/com/example/irisha/task_1/FormActivity.java
@@ -3,6 +3,7 @@ package com.example.irisha.task_1;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
 
--- Task_1/app/src/main/java/com/example/irisha/task_1/MainActivity.java
+++ Task_1/app/src/main/java/com/example/irisha/task_1/MainActivity.java
@@ -2,8 +2,8 @@ package com.example.irisha.task_1;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
